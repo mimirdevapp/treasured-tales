da4305eab8570cc847d1bb010aac92e0b4f2b379
mimirdevapp <mimir.dev.app@gmail.com>
2025-12-15 18:22:25 +0530

    route error handling

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Gallery from './pages/Gallery';
 import Contact from './pages/Contact';
 import Article from './pages/Article';
 import ScrollToTop from './components/ScrollToTop';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
             <Route path="/gallery" element={<Gallery />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/gallery/:id" element={<Article />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
         <WhatsAppButton />
